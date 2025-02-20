99ba9d525a00aa0010e84ff719f6e77094fecbd6
Roman Demidov <roman.demidov@onlyoffice.com>
2025-02-20 11:22:41 +0000

    Update to core24 (#9)
    Reviewed-on: https://git.onlyoffice.com/ONLYOFFICE/snap-desktopeditors/pulls/9
    Co-authored-by: Roman Demidov <roman.demidov@onlyoffice.com>
    Co-committed-by: Roman Demidov <roman.demidov@onlyoffice.com>

--- bin/run-desktopeditors.sh
+++ bin/run-desktopeditors.sh
@@ -101,7 +101,7 @@ fi
 #Create cache for input methods
 export GTK_IM_MODULE_FILE=$XDG_CACHE_HOME/immodules.cache
 if [ ! -f $GTK_IM_MODULE_FILE ]; then
-  $SNAP/usr/lib/x86_64-linux-gnu/libgtk-3-0/gtk-query-immodules-3.0 --update-cache
+  $SNAP/usr/lib/x86_64-linux-gnu/libgtk-3-0t64/gtk-query-immodules-3.0 --update-cache
 fi
 
 unset SESSION_MANAGER
